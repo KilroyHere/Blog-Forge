13c701dfc1fab5ac3bee2354404d7f3b0299dbe1
Aryan Patel <66572285+KilroyHere@users.noreply.github.com>
2025-12-23 03:08:32 -0800

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Dashboard from "@/pages/Dashboard";
+import EditorPage from "@/pages/EditorPage";
+import PostView from "@/pages/PostView";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/new" component={EditorPage} />
+      <Route path="/edit/:id" component={EditorPage} />
+      <Route path="/post/:id" component={PostView} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
